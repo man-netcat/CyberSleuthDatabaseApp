105e6172fca25358364e41aaee5933fd629276d0
Rick Teuthof <rmt22198@outlook.com>
2022-02-20 21:05:12 +0100

    Added required stats to path

--- app/src/main/java/com/example/cybersleuthpathfinder/PathActivity.java
+++ app/src/main/java/com/example/cybersleuthpathfinder/PathActivity.java
@@ -11,16 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class PathActivity extends AppCompatActivity {
 
     ArrayList<String> resList = new ArrayList<>();
     ArrayList<String> names = new ArrayList<>();
-    ArrayList<SrcDigimon> digimon_database = new ArrayList<>();
+    ArrayList<SrcDigimon> digimonDatabase = new ArrayList<>();
+    HashMap<String, HashMap<String, DstDigimon>> digimonMap = new HashMap<>();
     Pathfinder pathfinder;
 
     /* Get digimon database from JSON */
@@ -29,14 +33,25 @@ public class PathActivity extends AppCompatActivity {
         Type listDigimonType = new TypeToken<List<SrcDigimon>>() {
         }.getType();
         Gson gson = new Gson();
-        digimon_database = gson.fromJson(jsonFileString, listDigimonType);
-        assert digimon_database != null;
-        pathfinder = new Pathfinder(digimon_database);
+        digimonDatabase = gson.fromJson(jsonFileString, listDigimonType);
+        assert digimonDatabase != null;
+        for (SrcDigimon srcDigimon : digimonDatabase) {
+            HashMap<String, DstDigimon> digimonDstMap = new HashMap<>();
+            for (DstDigimon dstDigimon : srcDigimon.Next) {
+                digimonDstMap.put(dstDigimon.Name, dstDigimon);
+            }
+            for (String prevDigimon : srcDigimon.Prev) {
+                digimonDstMap.put(prevDigimon, null);
+            }
+            digimonMap.put(srcDigimon.Name, digimonDstMap);
+        }
+
+        pathfinder = new Pathfinder(digimonDatabase);
     }
 
     /* Get list of names from database */
     void populateDatabase() {
-        for (SrcDigimon digimon : digimon_database) {
+        for (SrcDigimon digimon : digimonDatabase) {
             names.add(digimon.Name);
         }
         Collections.sort(names);
@@ -73,21 +88,43 @@ public class PathActivity extends AppCompatActivity {
         /* Set button for finding path */
         Button button = findViewById(R.id.calculate_path_button);
         button.setOnClickListener(v -> {
-            String src = srcView.getText().toString();
-            String dst = dstView.getText().toString();
+            try {
+                String src = srcView.getText().toString();
+                String dst = dstView.getText().toString();
 
-            resList.clear();
+                resList.clear();
 
-            ArrayList<String> path = pathfinder.findPath(src, dst);
+                ArrayList<String> path = pathfinder.findPath(src, dst);
 
-            for (int i = 0; i < path.size() - 1; i++) {
-                String cur = path.get(i);
-                String next = path.get(i + 1);
-                String str = cur + " -> " + next;
-                resList.add(str);
+                for (int i = 0; i < path.size() - 1; i++) {
+                    String cur = path.get(i);
+                    String next = path.get(i + 1);
+
+                    DstDigimon stats = Objects.requireNonNull(digimonMap.get(cur)).get(next);
+                    StringBuilder statStr;
+                    if (stats == null) {
+                        statStr = new StringBuilder("\nDe-Digivolve");
+                    } else {
+                        statStr = new StringBuilder();
+                        Field[] fields = stats.getClass().getDeclaredFields();
+
+                        for (Field field : fields) {
+                            field.setAccessible(true);
+                            String name = field.getName();
+                            if (name.equals("Name")) continue;
+                            Object value = field.get(stats);
+                            if (value != null)
+                                statStr.append("\n").append(name).append(": ").append(value);
+                        }
+                    }
+
+                    String str = cur + " -> " + next + statStr;
+                    resList.add(str);
+                }
+
+                resAdapter.notifyDataSetChanged();
+            } catch (Exception ignored) {
             }
-
-            resAdapter.notifyDataSetChanged();
         });
     }
 }
